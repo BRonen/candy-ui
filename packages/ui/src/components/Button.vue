0187f990190852c49030ea09314aac0256b71515
Brenno Rodrigues <brennopereira6@gmail.com>
2023-11-18 04:18:50 -0300

    style: formatting vue components

--- packages/ui/src/components/Button.vue
+++ packages/ui/src/components/Button.vue
@@ -1,73 +1,76 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { computed } from "vue";
 
-const props = withDefaults(defineProps<{
-    label: string,
-    primary?: boolean,
-    size?: 'small' | 'medium' | 'large',
-    backgroundColor?: string,
-
-}>(), { primary: false });
+const props = withDefaults(
+  defineProps<{
+    label: string;
+    primary?: boolean;
+    size?: "small" | "medium" | "large";
+    backgroundColor?: string;
+  }>(),
+  { primary: false }
+);
 
 const emit = defineEmits<{
-    (e: 'click'): void;
+  (e: "click"): void;
 }>();
 
 const classes = computed(() => ({
-    'storybook-button': true,
-    'storybook-button--primary': props.primary,
-    'storybook-button--secondary': !props.primary,
-    [`storybook-button--${props.size || 'medium'}`]: true,
+  "storybook-button": true,
+  "storybook-button--primary": props.primary,
+  "storybook-button--secondary": !props.primary,
+  [`storybook-button--${props.size || "medium"}`]: true,
 }));
 
 const style = computed(() => ({
-    backgroundColor: props.backgroundColor
+  backgroundColor: props.backgroundColor,
 }));
 
 const onClick = () => {
-    emit("click")
+  emit("click");
 };
-
 </script>
 
 <template>
-    <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
+  <button type="button" :class="classes" @click="onClick" :style="style">
+    {{ label }}
+  </button>
 </template>
-  
+
 <style scoped>
 .storybook-button {
-    font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    font-weight: 700;
-    border: 0;
-    border-radius: 3em;
-    cursor: pointer;
-    display: inline-block;
-    line-height: 1;
+  font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
 }
 
 .storybook-button--primary {
-    color: white;
-    background-color: #1ea7fd;
+  color: white;
+  background-color: #1ea7fd;
 }
 
 .storybook-button--secondary {
-    color: #333;
-    background-color: transparent;
-    box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+  color: #333;
+  background-color: transparent;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
 }
 
 .storybook-button--small {
-    font-size: 12px;
-    padding: 10px 16px;
+  font-size: 12px;
+  padding: 10px 16px;
 }
 
 .storybook-button--medium {
-    font-size: 14px;
-    padding: 11px 20px;
+  font-size: 14px;
+  padding: 11px 20px;
 }
 
 .storybook-button--large {
-    font-size: 16px;
-    padding: 12px 24px;
+  font-size: 16px;
+  padding: 12px 24px;
 }
 </style>
--- packages/ui/src/components/Test.vue
+++ packages/ui/src/components/Test.vue
@@ -1,17 +1,19 @@
 <script lang="ts" setup>
-import { reactive, watch } from 'vue';
+import { reactive, watch } from "vue";
 
 const counter = reactive({ value: 0 });
 
 watch([counter], () => {
-    console.log(counter.value, 'wasd')
+  console.log(counter.value, "wasd");
 });
 
 const increment = () => {
-    counter.value += 1;
-}
+  counter.value += 1;
+};
 </script>
 <template>
-    <button @click="increment" class="bg-red-400">Click test - {{ counter.value }}</button>
-    <h1>{{ counter.value }}</h1>
+  <button @click="increment" class="bg-red-400">
+    Click test - {{ counter.value }}
+  </button>
+  <h1>{{ counter.value }}</h1>
 </template>
